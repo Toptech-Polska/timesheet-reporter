e8cbd51e4223f7fbd07814ff0451fff19e84067d
PiotrSkopowski <piotr@skopowski.pl>
2026-04-25 08:00:01 +0200

    fix: dodaj supportsAllDrives=true dla dysków współdzielonych

--- app/actions/googleDrive.ts
+++ app/actions/googleDrive.ts
@@ -135,7 +135,7 @@ export async function uploadPdfToGoogleDrive(
     "folder:", folderId);
 
   const response = await fetch(
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&supportsAllDrives=true",
     {
       method: "POST",
       headers: {
